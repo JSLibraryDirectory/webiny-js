8311c9687fd28efbdeac22374dde34bda00d314c
Adrian Smijulj <adrian1358@gmail.com>
2018-07-09 08:20:54 +0200

    WIP(material): export components.
    affects: webiny-client-ui-material

--- packages/webiny-client-ui-material/index.js
+++ packages/webiny-client-ui-material/index.js
@@ -1,2 +1,12 @@
 // @flow
-// Does not export anything (for now).
+
+export { default as Button } from "./src/Button";
+export { default as Checkbox } from "./src/Checkbox";
+export { default as Dialog } from "./src/Dialog";
+export { default as Drawer } from "./src/Drawer";
+export { default as Icon } from "./src/Icon";
+export { default as Input } from "./src/Input";
+export { default as List } from "./src/List";
+export { default as Menu } from "./src/Menu";
+export { default as Radio } from "./src/Radio";
+export { default as Switch } from "./src/Switch";
---
+++ packages/webiny-client-ui-material/src/index.js
@@ -0,0 +1,12 @@
+// @flow
+
+export { default as Button } from "./Button";
+export { default as Checkbox } from "./Checkbox";
+export { default as Dialog } from "./Dialog";
+export { default as Drawer } from "./Drawer";
+export { default as Icon } from "./Icon";
+export { default as Input } from "./Input";
+export { default as List } from "./List";
+export { default as Menu } from "./Menu";
+export { default as Radio } from "./Radio";
+export { default as Switch } from "./Switch";
