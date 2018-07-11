c22ca2a988edc73b2efe0c1c784f04625d9b2da4
Adrian Smijulj <adrian1358@gmail.com>
2018-07-11 14:03:12 +0200

    fix: add missing exports in base index file.
    affects: webiny-client-ui-material

--- packages/webiny-client-ui-material/src/index.js
+++ packages/webiny-client-ui-material/src/index.js
@@ -1,12 +1,17 @@
 // @flow
-
 export { default as Button } from "./Button";
 export { default as Checkbox } from "./Checkbox";
 export { default as Dialog } from "./Dialog";
 export { default as Drawer } from "./Drawer";
+export { default as Elevation } from "./Elevation";
+export { default as Grid } from "./Grid";
 export { default as Icon } from "./Icon";
 export { default as Input } from "./Input";
 export { default as List } from "./List";
+export { default as ListData } from "./ListData";
 export { default as Menu } from "./Menu";
 export { default as Radio } from "./Radio";
+export { default as Ripple } from "./Ripple";
+export { default as Select } from "./Select";
+export { default as Slider } from "./Slider";
 export { default as Switch } from "./Switch";
