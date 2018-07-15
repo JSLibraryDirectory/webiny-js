04719f7f020733604b7e3cbc90ddbcb6e6d3a882
Adrian Smijulj <adrian1358@gmail.com>
2018-07-15 16:51:40 +0200

    fix(entity / form actions): make sure actions are working correctly with GraphQL actions.
    affects: webiny-client

--- packages/webiny-client/src/actions/entity.actions.js
+++ packages/webiny-client/src/actions/entity.actions.js
@@ -1,13 +1,20 @@
 // @flow
 import { createAction } from "./../redux";
-import { generateListQuery } from "./entity";
-import { generateFindOneQuery } from "./entity";
-import { graphql } from ".";
+import { graphqlQuery, graphqlMutation } from ".";
+import {
+    generateListQuery,
+    generateFindOneQuery,
+    generateUpdateQuery,
+    generateCreateQuery
+} from "./entity";
 
 const PREFIX = "[ENTITY]";
 
 export const ENTITY_LIST = `${PREFIX} List`;
 export const ENTITY_FIND = `${PREFIX} Find`;
+export const ENTITY_SAVE = `${PREFIX} Save`;
+export const ENTITY_CREATE = `${PREFIX} Create`;
+export const ENTITY_UPDATE = `${PREFIX} Update`;
 
 const listEntities = createAction(ENTITY_LIST, {
     middleware({ action, next }) {
@@ -15,7 +22,7 @@ const listEntities = createAction(ENTITY_LIST, {
 
         const { entity, fields, variables, onSuccess, onError } = action.payload;
         const generatedQuery = generateListQuery({ entity, fields });
-        graphql({
+        graphqlQuery({
             ...generatedQuery,
             variables,
             onSuccess: data => {
@@ -32,14 +39,13 @@ const listEntities = createAction(ENTITY_LIST, {
     }
 });
 
-const findOne = createAction(ENTITY_FIND, {
+const findOneEntity = createAction(ENTITY_FIND, {
     middleware({ action, next }) {
         next(action);
 
         const { entity, fields, variables, onSuccess, onError } = action.payload;
-        console.log(variables);
         const generatedQuery = generateFindOneQuery({ entity, fields });
-        graphql({
+        graphqlQuery({
             ...generatedQuery,
             variables,
             onSuccess: data => {
@@ -56,4 +62,63 @@ const findOne = createAction(ENTITY_FIND, {
     }
 });
 
-export { listEntities, findOne };
+const saveEntity = createAction(ENTITY_SAVE, {
+    middleware({ action, next }) {
+        next(action);
+        if (action.payload.data.id) {
+            updateEntity(action.payload);
+        } else {
+            createEntity(action.payload);
+        }
+    }
+});
+
+const createEntity = createAction(ENTITY_CREATE, {
+    middleware({ action, next }) {
+        next(action);
+        const { entity, fields, onSuccess, onError, data } = action.payload;
+        const generatedQuery = generateCreateQuery({ entity, fields });
+
+        graphqlMutation({
+            ...generatedQuery,
+            variables: { data, id: data.id },
+            onSuccess: data => {
+                if (typeof onSuccess === "function") {
+                    onSuccess({ ...data.data });
+                }
+            },
+            onError: error => {
+                if (typeof onError === "function") {
+                    onError({ ...error });
+                }
+            }
+        });
+    }
+});
+
+const updateEntity = createAction(ENTITY_UPDATE, {
+    middleware({ action, next }) {
+        next(action);
+        const { entity, fields, onSuccess, onError, data } = action.payload;
+        const generatedQuery = generateUpdateQuery({ entity, fields });
+
+        console.log(generatedQuery);
+
+        graphqlMutation({
+            ...generatedQuery,
+            variables: { data, id: data.id },
+            onSuccess: data => {
+                if (typeof onSuccess === "function") {
+                    onSuccess({ ...data.data });
+                }
+            },
+            onError: error => {
+                if (typeof onError === "function") {
+                    onError({ ...error });
+                }
+            }
+        });
+    }
+});
+
+export { listEntities, findOneEntity, createEntity, updateEntity, saveEntity };
--- packages/webiny-client/src/actions/form.actions.js
+++ packages/webiny-client/src/actions/form.actions.js
@@ -1,53 +1,16 @@
 // @flow
 import { createAction } from "./../redux";
-import { findOne } from ".";
+import { findOneEntity, saveEntity } from ".";
 
 const PREFIX = "[FORM]";
 
-export const FORM_SUBMIT = `${PREFIX} Submit`;
 export const FORM_SET_LOADING = `${PREFIX} Set Loading`;
 export const FORM_LOAD = `${PREFIX} Load`;
 export const FORM_LOAD_SUCCESS = `${PREFIX} Form Load Success`;
 export const FORM_LOAD_ERROR = `${PREFIX} Form Load Error`;
-
-const loadForm = createAction(FORM_LOAD, {
-    middleware({ action, next }) {
-        next(action);
-
-        const { name, entity, fields, id } = action.payload;
-        findOne({
-            entity,
-            fields,
-            variables: { id },
-            onSuccess: data => loadForm.success({ data, name }),
-            onError: error => loadForm.error({ error, name })
-        });
-    }
-});
-
-loadForm.success = createAction(FORM_LOAD_SUCCESS, {
-    selector: "forms",
-    reducer({ state = {}, action }) {
-        const { data, name } = action.payload;
-        state[name] = {
-            data,
-            error: null
-        };
-        return state;
-    }
-});
-
-loadForm.error = createAction(FORM_LOAD_ERROR, {
-    selector: "forms",
-    reducer({ state = {}, action }) {
-        const { error, name } = action.payload;
-        state[name] = {
-            error,
-            data: null
-        };
-        return state;
-    }
-});
+export const FORM_SUBMIT = `${PREFIX} Submit`;
+export const FORM_SUBMIT_SUCCESS = `${PREFIX} Form Submit Success`;
+export const FORM_SUBMIT_ERROR = `${PREFIX} Form Submit Error`;
 
 const setFormLoading = createAction(FORM_SET_LOADING, {
     selector: ({ action }) => {
@@ -60,10 +23,86 @@ const setFormLoading = createAction(FORM_SET_LOADING, {
     }
 });
 
+const loadForm = createAction(FORM_LOAD, {
+    middleware({ action, next }) {
+        next(action);
+
+        const { name, entity, fields, id } = action.payload;
+        findOneEntity({
+            entity,
+            fields,
+            variables: { id },
+            onSuccess: data => loadFormSuccess({ data, name }),
+            onError: error => loadFormError({ error, name })
+        });
+    }
+});
+
+const loadFormSuccess = createAction(FORM_LOAD_SUCCESS, {
+    selector: "forms",
+    reducer({ state = {}, action }) {
+        const { data, name } = action.payload;
+        state[name] = {
+            data,
+            error: null
+        };
+        return state;
+    }
+});
+
+const loadFormError = createAction(FORM_LOAD_ERROR, {
+    selector: "forms",
+    reducer({ state = {}, action }) {
+        const { error, name } = action.payload;
+        state[name] = {
+            error,
+            data: null
+        };
+        return state;
+    }
+});
+
 const submitForm = createAction(FORM_SUBMIT, {
     middleware({ action, next }) {
         next(action);
-        setFormLoading({ name: action.payload.name, loading: true });
+        const { name } = action.payload;
+        setFormLoading({ name, loading: true });
+
+        saveEntity({
+            ...action.payload,
+            onSuccess: data => {
+                submitFormSuccess({ data, name });
+                setFormLoading({ name, loading: false });
+            },
+            onError: error => {
+                submitFormError({ error, name });
+                setFormLoading({ name, loading: false });
+            }
+        });
+    }
+});
+
+const submitFormSuccess = createAction(FORM_SUBMIT_SUCCESS, {
+    selector: "forms",
+    reducer({ state = {}, action }) {
+        const { data, name } = action.payload;
+        state[name] = {
+            data,
+            error: null
+        };
+        return state;
+    }
+});
+
+const submitFormError = createAction(FORM_SUBMIT_ERROR, {
+    selector: "forms",
+    reducer({ state = {}, action }) {
+        const { error, name } = action.payload;
+        state[name] = {
+            error,
+            data: null
+        };
+        return state;
     }
 });
 
