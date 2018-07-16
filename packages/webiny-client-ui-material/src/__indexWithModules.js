bae0f7b956e143f9f2fd71ec6142daf01068c867
Adrian Smijulj <adrian1358@gmail.com>
2018-07-16 05:38:05 +0200

    WIP(material): uI components must be registered as modules.
    affects: webiny-client-ui-material

---
+++ packages/webiny-client-ui-material/src/__indexWithModules.js
@@ -0,0 +1,55 @@
+// @flow
+const app = () => {
+    return ({ app }, next) => {
+        app.modules.register([
+            {
+                name: "Button",
+                factory: () => import("./Button")
+            },
+            {
+                name: "Checkbox",
+                factory: () => import("./Checkbox")
+            },
+            {
+                name: "Form",
+                factory: () => import("webiny-form").then(m => m.Form)
+            },
+            {
+                name: "Grid",
+                factory: () => import("./Grid")
+            },
+            {
+                name: "Icon",
+                factory: () => import("./Icon")
+            },
+            {
+                name: "Input",
+                factory: () => import("./Input")
+            },
+            {
+                name: "Link",
+                factory: () => import("./Link")
+            },
+            {
+                name: "List",
+                factory: () => import("./List")
+            },
+            {
+                name: "Loader",
+                factory: () => import("./Loader")
+            },
+            {
+                name: "Select",
+                factory: () => import("./Select")
+            },
+            {
+                name: "Switch",
+                factory: () => import("./Switch")
+            },
+        ]);
+
+        next();
+    };
+};
+
+export { app };
