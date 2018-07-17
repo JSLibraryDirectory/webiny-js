2235e76c451cfee02e273515d9358167dea32775
Adrian Smijulj <adrian1358@gmail.com>
2018-07-17 11:10:06 +0200

    feat(hoc): implement withRouter HOC.
    affects: webiny-client

--- packages/webiny-client/src/hoc/withRouter.js
+++ packages/webiny-client/src/hoc/withRouter.js
@@ -1,68 +1,12 @@
 // @flow
-import { connect } from "react-redux";
-import { compose, lifecycle, withProps } from "recompose";
-import { loadList } from "./../actions";
-import _ from "lodash";
+import { compose, withProps } from "recompose";
+import { app } from "webiny-client";
 
-type WithFormParams = {
-    name: string,
-    entity: string,
-    fields: string
-};
-
-export default ({ name, entity, fields }: WithFormParams) => {
+export default () => {
     return BaseComponent => {
         return compose(
-            connect(state => {
-                return { list: _.get(state, `lists.${name}`, {}) };
-            }),
-            lifecycle({
-                componentDidMount() {
-                    loadList({
-                        name,
-                        entity,
-                        fields
-                    });
-                }
-            }),
-            withProps(props => {
-                props.list.refresh = () => {
-                    loadList({
-                        name,
-                        entity,
-                        fields
-                    });
-                };
-
-                let hasNext = false;
-                let hasPrevious = false;
-                if (props.list.data) {
-                    hasNext = props.list.data.meta.page < props.list.data.meta.totalPages;
-                    hasPrevious = props.list.data.meta.page > 1;
-                }
-
-                props.list.pages = {
-                    hasNext,
-                    hasPrevious,
-                    next: () => {
-                        hasNext &&
-                            loadList({
-                                name,
-                                entity,
-                                fields,
-                                page: props.list.data.meta.page + 1
-                            });
-                    },
-                    previous: () => {
-                        hasPrevious &&
-                            loadList({
-                                name,
-                                entity,
-                                fields,
-                                page: props.list.data.meta.page - 1
-                            });
-                    }
-                };
+            withProps(() => {
+                return { router: app.router };
             })
         )(BaseComponent);
     };
