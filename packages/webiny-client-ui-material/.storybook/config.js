f23c947ea46d1efd54f31f1b3628fd782af6a651
Pavel910 <pavel@webiny.com>
2018-07-29 16:16:00 +0200

    feat: add sass support
    affects: webiny-client-ui-material

--- packages/webiny-client-ui-material/.storybook/config.js
+++ packages/webiny-client-ui-material/.storybook/config.js
@@ -1,5 +1,5 @@
 import { configure } from "@storybook/react";
-import "material-components-web/dist/material-components-web.min.css";
+import "./../src/index.scss";
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /.stories.js$/);
--- packages/webiny-client-ui-material/.storybook/webpack.config.js
+++ packages/webiny-client-ui-material/.storybook/webpack.config.js
@@ -1,4 +1,21 @@
 module.exports = (config, env, defaultConfig) => {
     defaultConfig.module.rules[1].use = [{ loader: "raw-loader" }];
+    defaultConfig.module.rules.unshift({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+    });
+    defaultConfig.module.rules.push({
+        test: /\.scss$/,
+        loaders: [
+            "style-loader",
+            "css-loader",
+            {
+                loader: "sass-loader",
+                options: {
+                    includePaths: ["node_modules", "../../node_modules"]
+                }
+            }
+        ]
+    });
     return defaultConfig;
 };
